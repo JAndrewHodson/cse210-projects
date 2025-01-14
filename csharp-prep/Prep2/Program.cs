512e525c586f8b6d3978526aa9ecea07d8a5706e
Andrew <j.andrew.hodson@gmail.com>
2025-01-13 23:16:21 -0700

    Completed gradeing sorter if-else chain

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade as a percentage?");
+        string userInput = Console.ReadLine();
+        int grade = int.Parse(userInput);
+        string passFail = "pass";
+        string letterGrade = "A";
+
+        if (grade < 60)
+        {
+            letterGrade = "F";
+            passFail = "fail";
+
+        }
+        else if (grade < 70)
+        {
+            letterGrade = "D";
+            passFail = "fail";
+        }
+        else if (grade < 80)
+        {
+            letterGrade = "C";
+            passFail = "pass";
+        }
+        else if (grade < 90)
+        {
+            letterGrade = "B";
+            passFail = "pass";
+        }
+        else
+        {
+            letterGrade = "A";
+            passFail = "pass";
+        }
+        Console.WriteLine($"You got a {letterGrade}.");
+        if (passFail == "pass")
+        {
+            Console.WriteLine($"You {passFail} the course. Congratulations!");
+        } else 
+        {
+            Console.WriteLine($"You {passFail} the course. Better luck next time!");
+        }
     }
 }
